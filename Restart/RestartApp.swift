f1e681ad356c9d0ac63229f21f2f9863be1c098d
Thiago Furtado <magnothiago750@gmail.com>
2023-03-28 20:09:52 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Thiago Magno Furtado on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
